6b239d7ea7811a12eba612ea1270d2b012e5f8cf
Rawllings Kiprop <rutojr01@gmail.com>
2024-06-18 20:48:02 +0300

    Done with the game. Every component and funcyions are working

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,30 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const namePlayer = useRef();
+
+  const [playerName, setPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
+
+  // const handleChange = (e) => {
+  //   setPlayerName(e.target.value);
+  // };
+
+  const handleSubmit = () => {
+    setPlayerName(namePlayer.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          // value={playerName}
+          // onChange={handleChange}
+          ref={namePlayer}
+        />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
